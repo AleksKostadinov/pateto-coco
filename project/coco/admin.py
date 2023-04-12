32f0a1b6b473115848c1609d673ef293c5239610
AleksKostadinov <kostadinov12@gmail.com>
2023-04-13 01:51:07 +0300

    Made some changes to admin panel

--- project/coco/admin.py
+++ project/coco/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'author', 'published_on')
+    list_filter = ('status', 'published_on', 'author')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    # date_hierarchy = 'published_on'
+    ordering = ['status', '-published_on']
+
+
+admin.site.register(Post, PostAdmin)
