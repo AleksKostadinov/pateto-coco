6883bf71f09d5c77596833a40084d03d8e771ee9
AleksKostadinov <kostadinov12@gmail.com>
2023-04-13 01:35:12 +0300

    Add models; Also add them to the admin panel

--- project/coco/models.py
+++ project/coco/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+
+    STATUS = (
+        (0, 'Draft'),
+        (1, 'Published')
+    )
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    published_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-published_on']
+
+    def __str__(self):
+        return self.title
